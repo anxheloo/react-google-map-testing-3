713b439f6970fc20490d864395782cb6d7ebc6bd
Anxheloo <anxhelocenollari@gmail.com>
2024-05-24 10:30:43 +0200

    master branch

--- src/App.js
+++ src/App.js
@@ -8,15 +8,43 @@ import {
 } from "@vis.gl/react-google-maps";
 import { useCallback, useState } from "react";
 
-// RENDERIN A CUSTOM MAP WITH A CUSTOM ADVANCED MARKER WITH AN INFO WINDOW
+// RENDERIN A CUSTOM MAP WITH A CUSTOM ADVANCED MARKER WITH AN INFO
 
 function App() {
+  const [markerRef, marker] = useAdvancedMarkerRef();
+  const [infoWindowShown, setInfoWindowShown] = useState(false);
+  const handleMarkerClick = useCallback(
+    () => setInfoWindowShown((isShown) => !isShown),
+    []
+  );
+
   const position = { lat: 53.54, lng: 10 };
 
   return (
     <APIProvider apiKey="AIzaSyBP99Vf_gDpO4gDtVz96aVukYBn6fTChSw">
       <div style={{ height: "100vh", width: "100%" }}>
-        <Map zoom={9} center={position} mapId="a0f9dccaba48dc35"></Map>
+        <Map zoom={9} center={position} mapId="a0f9dccaba48dc35">
+          <AdvancedMarker
+            position={position}
+            onClick={handleMarkerClick}
+            ref={markerRef}
+          >
+            <Pin
+              background={"grey"}
+              borderColor={"black"}
+              glyphColor={"black"}
+            ></Pin>
+          </AdvancedMarker>
+
+          {infoWindowShown && (
+            <InfoWindow
+              anchor={marker}
+              onCloseClick={() => setInfoWindowShown((isShown) => !isShown)}
+            >
+              Im in Hamburg
+            </InfoWindow>
+          )}
+        </Map>
       </div>
     </APIProvider>
   );
